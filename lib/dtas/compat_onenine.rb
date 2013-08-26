3df12aeda7b480edf40da61f98cd2887a09eeb7b
Eric Wong <normalperson@yhbt.net>
2013-08-26 09:36:00 +0000

    :nodoc: core classes in 1.9 compat layer
    No we do not want to confuse `ri' users with this.

--- lib/dtas/compat_onenine.rb
+++ lib/dtas/compat_onenine.rb
@@ -5,13 +5,13 @@
 # Make Ruby 1.9.3 look like Ruby 2.0.0 to us
 # This exists for Debian wheezy users using the stock Ruby 1.9.3 install.
 # We'll drop this interface when Debian wheezy (7.0) becomes unsupported.
-class String
+class String # :nodoc:
   def b # :nodoc:
     dup.force_encoding(Encoding::BINARY)
   end
 end unless String.method_defined?(:b)
 
-def IO
+def IO # :nodoc:
   def self.pipe # :nodoc:
     super.map! { |io| io.close_on_exec = true; io }
   end
