aa639718901a1cf77e1a8f02d2ec613e29cca707
Eric Wong <normalperson@yhbt.net>
2013-08-31 06:05:33 +0000

    disclaimer: disambiguate between dtas/$PROGNAME
    The whole dtas project is committed to giving as much rope as users
    need!

--- lib/dtas/disclaimer.rb
+++ lib/dtas/disclaimer.rb
@@ -5,14 +5,14 @@ DTAS_PROGNAME = File.basename($0)
 DTAS_DISCLAIMER = <<EOF
 # WARNING!
 #
-# Ignorant or improper use of #{DTAS_PROGNAME} may lead to
-# data loss, hearing loss, and damage to audio equipment.
+# Ignorant or improper use of #{DTAS_PROGNAME}
+# may lead to data loss, hearing loss, and damage to audio equipment.
 #
 # Please read and understand the documentation of all commands you
 # attempt to configure.
 #
-# #{DTAS_PROGNAME} will never prevent you from doing stupid things.
+# dtas will never prevent you from doing stupid things.
 #
-# There is no warranty, the developers of #{DTAS_PROGNAME}
-# are not responsible for your actions.
+# There is no warranty, the developers of dtas are not responsible
+# for your actions.
 EOF
