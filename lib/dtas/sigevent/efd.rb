23b87be611b0f927fc361db22f018ce9dd3aeb29
Eric Wong <e@80x24.org>
2014-12-28 11:06:23 +0000

    sigevent/efd: minor simplifications
    This reduces memory overhead by a few bytes by eliminating
    constant lookups and unnecessary inclusions.

--- lib/dtas/sigevent/efd.rb
+++ lib/dtas/sigevent/efd.rb
@@ -1,10 +1,8 @@
 # Copyright (C) 2013-2014, Eric Wong <e@80x24.org> and all contributors
 # License: GPLv3 or later (https://www.gnu.org/licenses/gpl-3.0.txt)
 class DTAS::Sigevent < SleepyPenguin::EventFD # :nodoc:
-  include SleepyPenguin
-
   def self.new
-    super(0, EventFD::CLOEXEC)
+    super(0, :CLOEXEC)
   end
 
   def signal
