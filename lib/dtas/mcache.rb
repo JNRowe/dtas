6cc0ae1f3e365015fe219703ccab77b19db4705b
Eric Wong <e@80x24.org>
2022-01-24 08:43:55 +0000

    player: expire sox metadata cache on file st_ctime changes
    We still need the TTL to deal with fuse.sshfs and maybe other weird
    FSes which don't return the st_ctime properly.

--- lib/dtas/mcache.rb
+++ lib/dtas/mcache.rb
@@ -13,14 +13,25 @@ class DTAS::Mcache
 
   def lookup(infile)
     bucket = infile.hash & @mask
+    st = nil
     if cur = @tbl[bucket]
       if cur[:infile] == infile && (DTAS.now - cur[:btime]) < @ttl
-        return cur
+        begin
+          st = File.stat(infile)
+          return cur if cur[:ctime] == st.ctime
+        rescue
+        end
       end
     end
     return unless block_given?
     @tbl[bucket] = begin
       cur = cur ? cur.clear : {}
+      begin
+        st ||= File.stat(infile)
+        cur[:ctime] = st.ctime
+      rescue
+        return
+      end
       if ret = yield(infile, cur)
         ret[:infile] = infile.frozen? ? infile : -(infile.dup)
         ret[:btime] = DTAS.now
--- test/test_mcache.rb
+++ test/test_mcache.rb
@@ -1,19 +1,29 @@
-# Copyright (C) 2016-2020 all contributors <dtas-all@nongnu.org>
+# Copyright (C) all contributors <dtas-all@nongnu.org>
 # License: GPL-3.0+ <https://www.gnu.org/licenses/gpl-3.0.txt>
 # frozen_string_literal: true
 require './test/helper'
 require 'dtas/mcache'
+require 'tempfile'
 
 class TestMcache < Testcase
   def test_mcache
+    tmp = Tempfile.new(%W(tmp .sox))
+    fn = tmp.path
+    cmd = %W(sox -r 44100 -b 16 -c 2 -n #{fn} trim 0 1)
+    system(*cmd) or skip
     mc = DTAS::Mcache.new
     exist = nil
-    mc.lookup('hello') { |infile, hash| exist = hash }
+    mc.lookup(fn) { |infile, hash|
+      hash[:ctime] = File.stat(infile).ctime
+      exist = hash
+    }
     assert_kind_of Hash, exist
-    assert_equal 'hello', exist[:infile]
+    assert_equal fn, exist[:infile]
     assert_operator exist[:btime], :<=, DTAS.now
-    assert_same exist, mc.lookup('hello')
+    assert_same exist, mc.lookup(fn)
     assert_nil mc.lookup('HELLO')
-    assert_same exist, mc.lookup('hello'), 'no change after miss'
+    assert_same exist, mc.lookup(fn), 'no change after miss'
+  ensure
+    tmp.close!
   end
 end
